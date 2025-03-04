bc3cfde72055abb4da14c0883df9529be6562ac5
QuaRang <dbduddnd1225@gmail.com>
2025-03-04 19:18:21 +0900

    feature/#14 - 유저정보 요청 메서드 추가
    - users 컬렉션에서 userid와 일치한 문서를 불러와 해당 필드 내용을 모두 불러오는 매서드 구현
    - 만약 404에러가 뜨면, 유저를 찾을 수 없다는 문구 출력
    - 해당 데이터를 기존에 새성한 User DTO로 디코딩해 반환

--- project-team-card-app/Maanger/UserAPIService.swift
+++ project-team-card-app/Maanger/UserAPIService.swift
@@ -8,7 +8,20 @@
 import Foundation
 
 class UserAPIService{
-    static func user()->User{
-        let 
+    
+    private static let db = Firestore.firestore()
+    
+    static func fetchUser(userId:String) async throws -> User{
+        let ref = db.collection("users").document(userId)
+        
+        let snapshot = try await ref.collection("contents").getDocuments()
+        do {
+            let snapshot = try await ref.getDocument()
+            guard snapshot.exists else { throw NSError(domain: "FirestoreService", code: 404, userInfo: [NSLocalizedDescriptionKey: "User not found"]) }
+            
+            return try snapshot.data(as: User.self)
+        } catch {
+            throw error
+        }
     }
 }
