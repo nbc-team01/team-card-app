08fde3628bf3eee1578396ae1cd570889f87ca17
QuaRang <dbduddnd1225@gmail.com>
2025-03-04 13:28:16 +0900

    feature/#9 - 테스트 코드 작성
    - 커스텀 데이터 테스크 코드 작성

--- project-team-card-appTests/project_team_card_appTests.swift
+++ project-team-card-appTests/project_team_card_appTests.swift
@@ -13,5 +13,16 @@ struct project_team_card_appTests {
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-
+    //유저 커스텀 데이터 테스트 코드
+    @Test func getUserData() async throws{
+        let user = CustomData.shared.user
+        print("user : \(user)")
+        #expect(user.userID == "UUID")
+    }
+    //유저 리스트 커스텀 데이터 테스트 코드
+    @Test func getUserListData() async throws{
+        let list = CustomData.shared.userList
+        print("user : \(list[1])")
+        #expect(list[1].userID == "UUID1")
+    }
 }
