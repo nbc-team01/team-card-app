ee0745997100fcc779ba85af8ca8fc6b6a0655a9
QuaRang <dbduddnd1225@gmail.com>
2025-03-04 19:13:46 +0900

    feature/#14 - 세팅 내용 적용
    - GoogleService-Info파일 추가
    - UserAPIService 파일 생성

---
+++ project-team-card-app/Maanger/UserAPIService.swift
@@ -0,0 +1,14 @@
+//
+//  UserAPIService.swift
+//  project-team-card-app
+//
+//  Created by 유영웅 on 3/4/25.
+//
+
+import Foundation
+
+class UserAPIService{
+    static func user()->User{
+        let 
+    }
+}
