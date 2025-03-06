24f47b8f0922e98629756782b1cf455cd27ed68e
QuaRang <dbduddnd1225@gmail.com>
2025-03-07 03:55:40 +0900

    feature/#40-앱 아이콘 및 스플래시 뷰 구현

--- project-team-card-app/Splash/SplashViewController.swift
+++ project-team-card-app/Splash/SplashViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class SplashViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         configure()
     }
     
